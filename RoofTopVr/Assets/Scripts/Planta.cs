4609914cfecc53d7453015b6816923670c1ffb6f
Ángel López <62568478+angelo066@users.noreply.github.com>
2022-11-18 19:26:13 +0100

    No falles por dios

---
+++ RoofTopVr/Assets/Scripts/Planta.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum tipo { Monstera, Peyote};
+public class Planta : MonoBehaviour
+{
+    tipo tipo_;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
