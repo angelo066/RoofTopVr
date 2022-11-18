39bfda64a077ad797afadb8363b20742ecde32c7
Ángel López <angelo06@ucm.es>
2022-11-18 20:53:26 +0100

    Calentador funcionando

--- RoofTopVr/Assets/Scripts/Plants/Planta.cs
+++ RoofTopVr/Assets/Scripts/Plants/Planta.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 //Tipos de plantas que tenemos en el juego (Añadir aqui si se quieren meter mas)
 public enum tipo { Monstera, Violeta, Corona, Tomatera, Anturio, Jade, Peyote};
+
 public class Planta : MonoBehaviour
 {
+    //Cuanto calienta el radiador, si se modifica aqui se modifica en todas
+    private const  int potenciaCalentador = 20;
+
+    private const  int temperaturaTerraza = 21;     //La temperatura que haría en la terraza
+
     //Representa la planta que es
     public tipo tipo_;
     //Representa como de dificil es para la planta PERDER agua
@@ -15,10 +21,10 @@ public class Planta : MonoBehaviour
     public int resistencia;
 
     //A que temperatura esta ahora mismpo la planta
-    private int temperatura_Actual;
+    private int temperatura_Actual = temperaturaTerraza;        //En grados centigrados
 
     //Como de humedad esta ahora mismo la planta
-    private int humedad_Actual = 0;                 //Puede ser que estas inicializaciones haya que cambiarlas más tarde
+    private int humedad_Actual = 0;                 //Puede ser que estas inicializaciones haya que cambiarlas más tarde (Son los valores con los que empiezan las plantas)
 
     //Cuanta agua tiene la planta ahora mismo
     private int agua_Actual = 0;
@@ -33,4 +39,26 @@ public class Planta : MonoBehaviour
     public int [] rangoHumedad = new int[2];
     public int [] rangoAgua = new int[2];
 
+    //Será el objeto de la escena 
+    GameObject calentador;
+
+    //Para que sepamos si el calentador/humificador tienen que funcionar o no
+    bool enInvernadero = true;
+
+    private void Update()
+    {
+        //Como de lejos estamos del radiador
+        Vector3 DistRad = transform.position - calentador.transform.position;
+
+        //Si esta en el invernadero le subimos la temperatura en funcion a como de cerca esta del calentador (MAXIMA = temperatura_Actual, ahora mismo 20, + potenciaCalentador)
+        if (enInvernadero)
+        {
+            temperatura_Actual = temperaturaTerraza + (potenciaCalentador - (int)DistRad.magnitude);
+        }
+    }
+
+    private void Start()
+    {
+        calentador = GameObject.FindGameObjectWithTag("Calentador");
+    }
 }
